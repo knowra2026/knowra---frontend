b441bc7593477b4f7dc1cd588e150d719abd36dd
Santhosh-1218 <boppudisanthosh44@gmail.com>
2025-12-01 13:37:25 +0530

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: 'hsl(var(--primary))',
+        'primary-glow': 'hsl(var(--primary-glow))',
+        accent: 'hsl(var(--accent))',
+        'accent-bright': 'hsl(var(--accent-bright))',
+        muted: 'hsl(var(--muted))',
+        card: 'hsl(var(--card))',
+        'card-dark': 'hsl(var(--card-dark))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))'
+      },
+      borderColor: theme => ({
+        // map `border-border` -> hsl(var(--border))
+        border: 'hsl(var(--border))',
+        DEFAULT: theme('colors.border')
+      })
+    },
   },
   plugins: [],
 }
